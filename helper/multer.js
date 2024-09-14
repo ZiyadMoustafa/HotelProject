257e0723e2e874fd12f8076a46b58bfc55a60b0f
ZiyadMoustafa <149652535+ZiyadMoustafa@users.noreply.github.com>
2024-09-14 23:29:01 +0300

    Update multer.js

--- helper/multer.js
+++ helper/multer.js
@@ -1,57 +1,57 @@
-const cloudinary = require("cloudinary").v2
-const multer = require('multer')
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+require("dotenv").config();
 
 cloudinary.config({
-    cloud_name: 'dbbzndvyn',
-    api_key: 971649224713466,
-    api_secret: 'ZRGPapwP1W46VSGIQ-ouu9VvQGk'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const multerStorage = multer.memoryStorage()
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image")) {
-        cb(null, true)
-    } else {
-        cb(new appError('not an image ! please upload only images..', 400), false)
-    }
-}
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Not an image! Please upload only images."), false);
+  }
+};
 
 const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter
-})
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
-exports.uploadPhoto = upload.single('hotelImage') //      <-------
+exports.uploadPhoto = upload.single("hotelImage"); //      <-------
 
 exports.resizePhotoProject = async (req, res, next) => {
+  if (!req.file) return next();
 
-    if (!req.file) return next()
+  const fileName = `${req.file.originalname}`;
 
-    const fileName = `${req.file.originalname}`
+  const filePath = `Hotels`; //    <-------
 
-    const filePath = `Hotels` //    <-------
+  const result = await uploadToClodinary(req.file.buffer, fileName, filePath);
+  req.body.hotelImage = result.secure_url; //    <-------
 
-    const result = await uploadToClodinary(req.file.buffer, fileName, filePath)
-    req.body.hotelImage = result.secure_url //    <-------
-
-    next()
-}
+  next();
+};
 
 const uploadToClodinary = (buffer, filename, folderPath, options = {}) => {
-    return new Promise((resolve, reject) => {
-        options.folder = folderPath;
-        options.public_id = filename;
-        const uploadStream = cloudinary.uploader.upload_stream(
-            options,
-            (error, result) => {
-                if (error) {
-                    reject(error)
-                } else {
-                    resolve(result)
-                }
-            }
-        )
-        uploadStream.end(buffer)
-    })
-}
+  return new Promise((resolve, reject) => {
+    options.folder = folderPath;
+    options.public_id = filename;
+    const uploadStream = cloudinary.uploader.upload_stream(
+      options,
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+    uploadStream.end(buffer);
+  });
+};
