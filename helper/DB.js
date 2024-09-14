1ffe7df05e12bff0370249ea94eb140576a567ba
ZiyadMoustafa <149652535+ZiyadMoustafa@users.noreply.github.com>
2024-09-14 23:28:40 +0300

    Update DB.js

--- helper/DB.js
+++ helper/DB.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const db = () => {
-
-    mongoose.connect("mongodb+srv://ziyadmoo:zimo159753@cluster0.82hvfib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",)
-        .then(() => console.log('DB now is connected'))
-        .catch((err) => { console.error('Could not connect to MongoDB', err); });
+  const uri = process.env.MONGODB_URI;
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB now is connected"))
+    .catch((err) => {
+      console.error("Could not connect to MongoDB", err);
+    });
 };
 
 module.exports = { db };
-
-//mongodb://0.0.0.0:27017/
